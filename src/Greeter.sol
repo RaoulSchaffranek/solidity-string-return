61d51b241126e2a28819448523eb12f60bfec5e7
Raoul <1412344+RaoulSchaffranek@users.noreply.github.com>
2025-05-09 18:39:17 +0000

    Added simple greeter

---
+++ src/Greeter.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Greeter {
+
+    function greeting() external pure returns (string memory) {
+        return "Hello, World!";
+    }
+}
